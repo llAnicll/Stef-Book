7bd7a739f0b7d81412b8be26ede52d9fbc3ac90f
Cailan Lay <Cailanlay@gmail.com>
2020-10-09 16:57:57 -0600

    accidental edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from 'components/Nav';
 import Header from 'components/Header';
 import About from 'components/About';
@@ -9,49 +9,62 @@ import firebase from 'fbConfig';
 import CreateNewEventDialog from 'components/NewEventDialog';
 
 function App() {
-  const [user , setUser] = useState(null);
+  const [user, setUser] = useState(null);
   const [dialogState, setDialogState] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [createDialogState, setCreateDialogState] = useState(false);
   const [admin, setAdmin] = useState(false);
-  
+
   // toggle the dialog state
-  const toggleDialogState = item => {
+  const toggleDialogState = (item) => {
     setSelectedEvent(item);
-    setDialogState(prevState => !prevState)
-  }
-  
+    setDialogState((prevState) => !prevState);
+  };
+
   // toggle create dialog
   const toggleCreateDialogState = () => {
-    setCreateDialogState(prevState => !prevState);
-  }
+    setCreateDialogState((prevState) => !prevState);
+  };
 
   // listen for auth state change
   useEffect(() => {
     firebase.auth().onAuthStateChanged(setUser);
-  },[])
+  }, []);
 
   // this is how you get the admin cliem
   useEffect(() => {
-    if(user !== null) {
-      user.getIdTokenResult().then(idTokenResult => {
-        setAdmin(idTokenResult.claims.admin)
-      })
+    if (user !== null) {
+      user.getIdTokenResult().then((idTokenResult) => {
+        setAdmin(idTokenResult.claims.admin);
+      });
     } else {
       setAdmin(false);
     }
-  },[user])
+  }, [user]);
 
   return (
     <div>
       <Nav user={user} admin={admin} toggleCreate={toggleCreateDialogState}>
-        <Header/>
+        <Header />
         <Booking user={user} toggleDialog={toggleDialogState} />
-        <About/>
-        <Contact/>
+        <About />
+        <Contact />
       </Nav>
-      {selectedEvent && <ConfirmDialog open={dialogState} user={user} item={selectedEvent} toggle={toggleDialogState} admin={admin}/>}
-      {admin && <CreateNewEventDialog open={createDialogState} toggle={toggleCreateDialogState} />}
+      {selectedEvent && (
+        <ConfirmDialog
+          open={dialogState}
+          user={user}
+          item={selectedEvent}
+          toggle={toggleDialogState}
+          admin={admin}
+        />
+      )}
+      {admin && (
+        <CreateNewEventDialog
+          open={createDialogState}
+          toggle={toggleCreateDialogState}
+        />
+      )}
     </div>
   );
 }
